560aa34c0d05415462e9660c55f908f42794c086
pjochillin <pjochillin@gmail.com>
2020-07-31 17:25:20 -0400

    Update ListSingleTagBase.java
    link ListTag obj. lang

--- src/main/java/com/denizenscript/denizencore/tags/core/ListSingleTagBase.java
+++ src/main/java/com/denizenscript/denizencore/tags/core/ListSingleTagBase.java
@@ -13,6 +13,7 @@ public class ListSingleTagBase {
         // @description
         // Returns a ListTag object with exactly 1 entry: whatever the input value is (even if that input is a list).
         // This is primarily useful for creating lists-within-lists.
+        // Refer to <@link language ListTag objects>
         // -->
         TagManager.registerTagHandler("list_single", (attribute) -> {
             if (!attribute.hasContext(1)) {
